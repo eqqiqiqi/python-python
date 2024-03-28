5682044c5ad8723026ccac212ae9534c53cf627e
eqqiqiqi <marijadressover@gmail.com>
2024-03-28 21:51:00 +0700

    Aritmatika

---
+++ aritmatika.py
@@ -0,0 +1,15 @@
+penjumlahan = 9 + 9
+
+pengurangan = 30 - 4
+
+pembagian = 20 / 2
+
+perkalian = 20 * 20
+
+campur_campur = 43 * 23 + (12 / 2) - 60
+
+print(penjumlahan)
+print(pengurangan)
+print(pembagian)
+print(perkalian)
+print(campur_campur)
